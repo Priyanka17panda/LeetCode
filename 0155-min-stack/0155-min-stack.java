4b977f80b8c55e63bb7f71c843e615c6edd3d4f7
Priyanka Panda <148116130+Priyanka17panda@users.noreply.github.com>
2025-07-28 18:59:03 +0530

    Time: 5 ms (38.13%), Space: 45.2 MB (24.83%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,35 +1,26 @@
 class MinStack {
-
     private Deque<Integer> stack = new ArrayDeque<>();
-    // minValuesStack keeps track of the minimum values at each state of the stack.
     private Deque<Integer> minValuesStack = new ArrayDeque<>();
 
     public MinStack() {
         minValuesStack.push(Integer.MAX_VALUE);
     }
-    
     public void push(int val) {
         stack.push(val);
-        // Push the smaller between the current value and the current minimum onto the minValuesStack.
         minValuesStack.push(Math.min(val, minValuesStack.peek()));
         
     }
-    
     public void pop() {
         stack.pop();
-        // Remove the top element of the minValuesStack, which corresponds to the minimum at that state.
         minValuesStack.pop();
     }
-    
     public int top() {
         return stack.peek();
     }
-    
     public int getMin() {
         return minValuesStack.peek();
     }
 }
-
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
