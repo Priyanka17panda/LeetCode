2737c3eb06254c03f55db1bd8feec9e702db4d5c
Priyanka Panda <148116130+Priyanka17panda@users.noreply.github.com>
2025-08-07 17:56:43 +0530

    Time: 1 ms (92.42%), Space: 45.9 MB (18.84%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sum=0;
+        int lsum=0;
+        for(int i=0;i<nums.length;i++){
+            sum=sum+nums[i];
+        }
+        for(int i=0;i<nums.length;i++){
+            if(lsum==sum-lsum-nums[i]){
+                return i;
+            }
+            lsum=lsum+nums[i];
+        }
+        return -1;
+    }
+}
