d79f732f479e764d08959a1ee2fd8ec204c1cd3b
Priyanka Panda <148116130+Priyanka17panda@users.noreply.github.com>
2025-09-10 19:35:16 +0530

    Time: 6 ms (50.15%), Space: 44.5 MB (81.87%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0, y = 0; 
+        for (int i = 0; i < moves.length(); i++) {
+            char c = moves.charAt(i);
+            switch (c) {
+                case 'U': y += 1; break;
+                case 'D': y -= 1; break;
+                case 'L': x -= 1; break;
+                case 'R': x += 1; break;
+                default:
+                    break;
+            }
+        }
+
+        return x == 0 && y == 0;
+    }
+}
