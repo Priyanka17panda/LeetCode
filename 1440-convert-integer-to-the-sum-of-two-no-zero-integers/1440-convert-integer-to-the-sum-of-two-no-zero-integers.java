e01b4dabd261a8bb727cc70001c8089c98f79594
Priyanka Panda <148116130+Priyanka17panda@users.noreply.github.com>
2025-09-19 01:08:55 +0530

    Time: 0 ms (100%), Space: 41.1 MB (90.75%) - LeetHub

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/1440-convert-integer-to-the-sum-of-two-no-zero-integers.java
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/1440-convert-integer-to-the-sum-of-two-no-zero-integers.java
@@ -1,18 +1,19 @@
 class Solution {
     public int[] getNoZeroIntegers(int n) {
-        for (int firstInteger = 1; ; firstInteger++) {
-            // Calculate the second integer as the difference
-            int secondInteger = n - firstInteger;
-          
-            // Convert both integers to strings and concatenate them
-            String concatenatedString = firstInteger + "" + secondInteger;
-          
-            // Check if the concatenated string contains the digit '0'
-            // If it doesn't contain '0', both integers are non-zero integers
-            if (!concatenatedString.contains("0")) {
-                // Return the pair of non-zero integers
-                return new int[] {firstInteger, secondInteger};
+        for (int i = 1; i < n; i++) { 
+            int ans = n - i;
+            if (check(i) && check(ans)) {
+                return new int[]{i, ans};
             }
         }
+        return new int[]{}; 
+    }
+
+    public boolean check(int a) {
+        while (a > 0) {
+            if (a % 10 == 0) return false; 
+            a /= 10;
+        }
+        return true;
     }
 }
