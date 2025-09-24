a87955ab5fa8a59e67645a2a8d69308a444ab039
Priyanka Panda <148116130+Priyanka17panda@users.noreply.github.com>
2025-09-24 22:48:57 +0530

    Time: 1 ms (99.8%), Space: 41.3 MB (28.64%) - LeetHub

---
+++ 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.java
@@ -0,0 +1,47 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public String fractionToDecimal(int numerator, int denominator) {
+        if (numerator == 0) {
+            return "0";
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        if ((numerator < 0) ^ (denominator < 0)) {
+            result.append("-");
+        }
+
+        long dividend = Math.abs((long) numerator);
+        long divisor = Math.abs((long) denominator);
+
+        result.append(dividend / divisor);
+        long remainder = dividend % divisor;
+
+        if (remainder == 0) {
+            return result.toString();
+        }
+
+        result.append(".");
+
+        Map<Long, Integer> remainderToPosition = new HashMap<>();
+
+        while (remainder != 0) {
+            if (remainderToPosition.containsKey(remainder)) {
+                int repeatStartPosition = remainderToPosition.get(remainder);
+                result.insert(repeatStartPosition, "(");
+                result.append(")");
+                return result.toString();
+            }
+            remainderToPosition.put(remainder, result.length());
+
+            remainder *= 10;
+            
+            result.append(remainder / divisor);
+            remainder %= divisor;
+        }
+
+        return result.toString();
+    }
+}
